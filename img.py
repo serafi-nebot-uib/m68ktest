fixed scaling method

img.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python3
 
+from __future__ import annotations
+
 import numpy as np
 from collections import deque
 from PIL import Image, ImageDraw
 
-def img_to_mat(path: str, psize: int) -> list:
+def img_to_mat(path: str, psize: int, width: int | None = None, height: int | None = None) -> list:
   img = Image.open(path)
 
   # resize width and height to be a multiple of psize
-  width, height = (s // psize * psize for s in img.size)
-  img = img.convert("RGBA")
-  arr = np.array(img)
+  size = s if None not in (s := (width, height)) else img.size
+  width, height = (x // psize * psize for x in size) # type: ignore[operator]
+  arr = np.array(img.resize((width, height), Image.Resampling.LANCZOS).convert("RGBA"))
 
   mat = []
   for y in range(0, height, psize):
@@ -22,7 +24,7 @@ def img_to_mat(path: str, psize: int) -> list:
     mat.append(row)
   return mat
 
-def mat_to_img(mat: list, psize: int) -> Image:
+def mat_to_img(mat: list, psize: int):
   width, height = len(mat[0]) * psize, len(mat) * psize
   img = Image.new("RGBA", (width, height))
   draw = ImageDraw.Draw(img)
@@ -31,18 +33,17 @@ def mat_to_img(mat: list, psize: int) -> Image:
       draw.rectangle((x*psize, y*psize, (x+1)*psize, (y+1)*psize), fill=tuple(color))
   return img
 
-def rec_to_img(rec: list, width: int, height: int) -> Image:
+def rec_to_img(rec: list, width: int, height: int):
   img = Image.new("RGBA", (width, height))
   draw = ImageDraw.Draw(img)
   for c, r in rec: draw.rectangle(r, fill=tuple(c))
   return img
 
-def mat_to_rec(mat: list) -> list:
+def mat_to_rec(mat: list, psize: int) -> list:
   rows, cols = len(mat[0]), len(mat)
   vis = [[False for _ in range(cols)] for _ in range(rows)]
-  rec = []
 
-  def flood_fill(x: list, y: list, color: list):
+  def flood_fill(x, y, color):
     q = deque([(x, y)])
     min_x, min_y, max_x, max_y = x, y, x, y
     while q:
@@ -52,36 +53,27 @@ def mat_to_rec(mat: list) -> list:
         min_x, min_y = min(min_x, curr_x), min(min_y, curr_y)
         max_x, max_y = max(max_x, curr_x), max(max_y, curr_y)
         for dx, dy in ((0, 1), (1, 0), (0, -1), (-1, 0)): q.append((curr_x + dx, curr_y + dy))
-    return (min_x, min_y, max_x, max_y)
+    return tuple(x*psize for x in (min_x, min_y, max_x+1, max_y+1))
 
-  for y in range(rows):
-    for x in range(cols):
-      if not vis[y][x] and mat[y][x][3] != 0:
-        r = flood_fill(x, y, mat[y][x])
-        rec.append((mat[y][x], r))
+  return [(mat[y][x], flood_fill(x, y, mat[y][x])) for y in range(rows) for x in range(cols) if not vis[y][x] and mat[y][x][3] > 128]
 
-  return rec
+WIDTH, HEIGHT = 64, 64
+PSIZE = 4
 
-PSIZE = 16
-
-def rec_to_m68k(color: list, coords: list):
+def rec_m68k_enc(color: list, coords: list) -> str:
   r, g, b, *_ = color
-  sx, sy, ex, ey, *_ = (x * PSIZE for x in coords) # manual resize to make it look bigger, shouldn't be done here
-  ex += PSIZE
-  ey += PSIZE
+  sx, sy, ex, ey, *_ = coords
   return f"dc.l  $00{b:02x}{g:02x}{r:02x}, ${sx:04x}{sy:04x}, ${ex:04x}{ey:04x}"
 
+def rec_to_m68k(rec: list) -> list: return [rec_m68k_enc(color, coords) for color, coords in rec]
+
 if __name__ == "__main__":
-  mat = img_to_mat("mario.png", PSIZE)
-  rec = mat_to_rec(mat)
-
-  # img = mat_to_img(mat, PSIZE)
-  # img.show()
-
+  mat = img_to_mat("/Users/hexdhog/Documents/UIB/CS/Y2/EC/m68ktest/tetris/blau.png", PSIZE, WIDTH, HEIGHT)
+  rec = mat_to_rec(mat, PSIZE)
   # print(rec)
-  # img = rec_to_img(rec, len(mat[0]), len(mat))
+  # img = rec_to_img(rec, WIDTH, HEIGHT)
   # img.show()
 
   # print(f"mat: {len(mat)*len(mat[0])}; rec: {len(rec)}")
 
-  for color, coords in rec: print(rec_to_m68k(color, coords))
+  print("\t" + "\n\t".join(rec_to_m68k(rec)))